2d366f4a251253441dff415a98d751992c417f4b
Jonas Hörberg <jonas@tinkerton.se>
2016-09-07 12:58:22 +0200

    File structure fixed

---
+++ public/js/client_app.js
@@ -0,0 +1,11 @@
+//react stuff here<script>
+
+  var socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
