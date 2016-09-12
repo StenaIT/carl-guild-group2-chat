e1de9b0f0c95c666623953297293611341cd3b79
Sebastian Nabrink <nabrink@Sebastians-MacBook-Pro.local>
2016-09-12 20:28:23 +0200

    Splitted up components into separate modules

---
+++ client/MessageList.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var Message = require('./Message');
+
+var MessageList = React.createClass({
+	render() {
+		return (
+			<div className='messages'>
+				<h2> Conversation: </h2>
+				{
+					this.props.messages.map((message, i) => {
+						return (
+							<Message
+								key={i}
+								user={message.user}
+								text={message.text}
+							/>
+						);
+					})
+				}
+			</div>
+		);
+	}
+});
+
+module.exports = MessageList;
